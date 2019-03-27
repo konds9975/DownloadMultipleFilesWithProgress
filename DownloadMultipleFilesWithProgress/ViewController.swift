82ab247677b18d0de23a4f1d0933d77b09c77683
kondya <konds9975@gmail.com>
2019-03-27 14:31:14 +0530

    Initial Commit

---
+++ DownloadMultipleFilesWithProgress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DownloadMultipleFilesWithProgress
+//
+//  Created by Kondya on 27/03/19.
+//  Copyright © 2019 Kondya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
